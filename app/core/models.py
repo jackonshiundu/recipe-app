d7f02329533e07d13486a71332abc876e252dde3
jackonshiundu <jackonshiundu2019@gmail.com>
2024-11-30 16:24:25 +0300

    model😁

--- app/core/models.py
+++ app/core/models.py
@@ -1,26 +1,28 @@
-from django.db import models # naqo
+from django.db import models  # naqo
 
-from django.contrib.auth.models import (AbstractBaseUser,BaseUserManager,PermissionsMixin)
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager, PermissionsMixin)
 
 
 class UserManager(BaseUserManager):
     """Manager for Users."""
 
     def create_user(self, email, password=None, **extra_fields):
-        user = self.model(email=email, **extra_fields)
+        """Create and return a user with an email and password."""
+        user = self.model(email=email, **extra_fields)  # Fixed: Removed extra spaces around '='
         user.set_password(password)
-        user.save(using = self._db)
+        user.save(using=self._db)
 
         return user
-class User(AbstractBaseUser, PermissionsMixin):
-    """User in the system"""
 
-    email = models.EmailField(max_length = 255, unique = True)
-    name = models.CharField(max_length = 255)
-    is_active = models.BooleanField(default = True)
-    is_stuff = models.BooleanField(default = True)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+
+    email = models.EmailField(max_length=255, unique=True)  # Fixed: Removed extra spaces around '='
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)  # Fixed: Removed extra spaces around '='
+    is_staff = models.BooleanField(default=True)  # Fixed: Changed 'is_stuff' to 'is_staff'
 
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-

03079f80604e59763b646954fc56f175a1cf1f8f
jackonshiundu <jackonshiundu2019@gmail.com>
2024-11-30 16:10:10 +0300

    modelchnages

--- app/core/models.py
+++ app/core/models.py
@@ -1,22 +1,28 @@
-from django.db import models # naqo
+from django.db import models  # naqo
 
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager, PermissionsMixin)
 
-from django.contrib.auth.models import (AbstractBaseUser,BaseUserManager,PermissionsMixin)
 
 class UserManager(BaseUserManager):
     """Manager for Users."""
-    def create_user(self,email,password=None,**extra_fields):
-        user=self.model(email=email,**extra_fields)
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and return a user with an email and password."""
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
-class User(AbstractBaseUser,PermissionsMixin):
-    """User in the system"""
-    email=models.EmailField(max_length=255,unique=True)
-    name=models.CharField(max_length=255)
-    is_active=models.BooleanField(default=True)
-    is_stuff=models.BooleanField(default=True)
 
-    objects=UserManager()
-    USERNAME_FIELD='email'
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
